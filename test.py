dcc6d0706ab088133ec0583aa84bf34958a14e13
JeramelR <122110341+SleeperJer@users.noreply.github.com>
2024-12-08 22:24:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import unittest
-
 class TestHolaMundo(unittest.TestCase):
     def test_hola_mundo(self):
         with open("index.html", "r") as file:
             content = file.read()
         self.assertIn("¡Hola Mundo!", content)
-
 if __name__ == "__main__":
     unittest.main()
